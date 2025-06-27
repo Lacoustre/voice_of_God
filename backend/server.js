f0140865ce1bee574b82a0f8d8f4b98a194439b2
Lacoustre <nkhprinceaboagye4944@gmail.com>
2025-06-26 21:35:37 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors({
-  origin: ['http://localhost:3001'],
+  origin: ['https://voice-of-god-1.onrender.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']

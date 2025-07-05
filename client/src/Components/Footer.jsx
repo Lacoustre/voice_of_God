f88b8f81f30a9d179aa7febe04866eabe52896c5
Lacoustre <nkhprinceaboagye4944@gmail.com>
2025-07-05 02:56:19 -0400

    Update Footer.jsx

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -50,7 +50,7 @@ const Footer = () => {
     try {
       await new Promise(resolve => setTimeout(resolve, 3000));
       
-      const response = await fetch("http://localhost:4000/api/contact", {
+      const response = await fetch("https://voice-of-god.onrender.com/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
